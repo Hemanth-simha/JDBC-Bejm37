d37c3880f83abfd92f1e9973b8aaa6f7ee3db2bf
Hemanth-simha <58566844+Hemanth-simha@users.noreply.github.com>
2020-05-22 11:14:09 +0530

    programs

---
+++ bejm37/Practice/src/com/jspiders/practice/second/ClassA.java
@@ -0,0 +1,17 @@
+package com.jspiders.practice.second;
+
+public class ClassA {
+	
+	public ClassA()
+	{
+		System.out.println("Constructor 1 Called");
+	}
+	public ClassA(int num)
+	{
+		System.out.println("Constructor 2 Called");
+	}
+	public ClassA(char type)
+	{
+		System.out.println("Constructor 3 Called");
+	}
+}
---
+++ bejm37/Practice/src/com/jspiders/practice/second/Mainclass.java
@@ -0,0 +1,14 @@
+package com.jspiders.practice.second;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		
+		new ClassA(); // o1
+		
+		new ClassA(10); // o2
+		
+		new ClassA('c'); // o3
+	}
+
+}
